384cde0cdc567abecdefce5fb93d7cb2885b58de
Maicol bautista <pirlo1121s@gmail.com>
2024-03-21 21:20:39 -0500

    {+} .env y docker compose

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require( 'dotenv' ).config();                               // Importa dependencia para crear variables de entorno
 
 const express = require( 'express' );                       // Importa Express crea infraestructura, rutas, responde a peticiones
-const { dbConection } = require('./config/mongo.config');   // Importa configuracion de DB para Mongo usando Mongoose
+const { dbConection } = require('./src/config/mongo.config');   // Importa configuracion de DB para Mongo usando Mongoose
 const app = express();                                      // Asigna invocacion de Express
 const cors = require( 'cors' );                             // CORS: Permisiona el acceso a peticiones de aplicaciones externas
-const { createDefaultUsers } = require('./config/mongo.setup');
+const { createDefaultUsers } = require('./src/config/mongo.setup');
 
 
 const PORT = process.env.PORT || 3000;                      // Define el puerto a traves de la variable de entorno
@@ -14,16 +14,16 @@ app.use( cors() );
 app.use( express.json() );
 
 /** Establece Rutas de la aplicacion */
-app.use( '/api/auth', require( './routes/auth.routes' ) );
+app.use( '/api/auth', require( './src/routes/auth.routes' ) );
 app.use(
     '/api/products',                        // Middleware que configura la ruta
-    require( './routes/products.routes' )   // Importa el archivo de rutas para esta ruta '/api/products'
+    require( './src/routes/products.routes' )   // Importa el archivo de rutas para esta ruta '/api/products'
 );    
-app.use( '/api/categories', require( './routes/category.routes' ) );
+app.use( '/api/categories', require( './src/routes/category.routes' ) );
 
-app.use('/api/services', require('./routes/services.routes'));
+app.use('/api/services', require('./src/routes/services.routes'));
 
-app.use('/api/talleres', require('./routes/taller.routes'));
+app.use('/api/talleres', require('./src/routes/taller.routes'));
 
 
 dbConection();      // Invoca la configuracion de DB, es decir: Pone a funcionar la BD
